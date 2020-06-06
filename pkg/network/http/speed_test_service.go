17ac1bfa2030452311b788b69d1244a96176678b
herocod3r <jethromain@gmail.com>
2020-06-06 02:40:44 +0100

    completed speed test service

--- pkg/network/http/speed_test_service.go
+++ pkg/network/http/speed_test_service.go
@@ -16,19 +16,78 @@ import (
 
 const (
 	configUrl = "https://www.speedtest.net/speedtest-config.php"
+	serverUrl = "https://www.speedtest.net/speedtest-servers-static.php"
 )
 
 type speedTestService struct {
 	ignoreIds []string
 }
 
-func (s *speedTestService) GetServers(max int, client network.Client) ([]network.Server, error) {
+func (s *speedTestService) GetServers(max int, client network.Client) (servers []network.Server, er error) {
 	if len(client.MetaData) < 1 {
 		return nil, errors.New("client metadata missing")
 	}
 
 	s.ignoreIds = strings.Split(client.MetaData, ",")
 
+	ul, _ := url.Parse(serverUrl)
+	queries := ul.Query()
+	queries.Add("X", time.Now().UTC().String())
+	ul.RawQuery = queries.Encode()
+	configEndpointUrl := ul.String()
+	rsp, er := http.Get(configEndpointUrl)
+	if er != nil {
+		er = &network.Error{InternalError: er}
+		return
+	}
+	defer rsp.Body.Close()
+	buf := new(bytes.Buffer)
+	_, er = buf.ReadFrom(rsp.Body)
+	if er != nil {
+		return
+	}
+
+	return s.parseServers(max, buf)
+
+}
+
+func (s *speedTestService) parseServers(max int, buffer *bytes.Buffer) (servers []network.Server, er error) {
+	servers = make([]network.Server, 0)
+	ignoreIdsMap := make(map[string]int)
+	for index, value := range s.ignoreIds {
+		ignoreIdsMap[value] = index
+	}
+
+	doc := etree.NewDocument()
+
+	count, er := doc.ReadFrom(buffer)
+	print(count)
+	if er != nil {
+		return
+	}
+
+	serversElm := doc.FindElement("//settings/servers")
+
+	if serversElm == nil {
+		er = errors.New("Unable to parse xml")
+		return
+	}
+
+	for _, serverElm := range serversElm.ChildElements() {
+		if len(servers) >= max {
+			break
+		}
+		id := serverElm.SelectAttrValue("id", "")
+		if _, ok := ignoreIdsMap[id]; ok {
+			continue
+		}
+		servers = append(servers, network.Server{
+			Name:    serverElm.SelectAttrValue("sponsor", ""),
+			Address: serverElm.SelectAttrValue("url", ""),
+		})
+	}
+
+	return
 }
 
 func (s *speedTestService) GetClientInfo() (client network.Client, er error) {
--- pkg/network/http/speed_test_service_test.go
+++ pkg/network/http/speed_test_service_test.go
@@ -14,4 +14,26 @@ func TestSpeedTestService_GetServers(t *testing.T) {
 	if len(client.Ip) < 1 || len(client.Isp) < 1 || client.Latitude == 0 || client.Longitude == 0 {
 		t.Fail()
 	}
+
+	servers, err := inst.GetServers(5, client)
+	if err != nil {
+		t.Fail()
+	}
+	if len(servers) != 5 {
+		t.Fail()
+	}
+}
+
+func TestSpeedTestService_GetClientInfo(t *testing.T) {
+	inst := speedTestService{}
+
+	client, er := inst.GetClientInfo()
+	if er != nil {
+		t.Fail()
+		return
+	}
+
+	if len(client.Ip) < 1 || len(client.Isp) < 1 || client.Latitude == 0 || client.Longitude == 0 {
+		t.Fail()
+	}
 }
